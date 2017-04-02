98a22547cf10583930ff62bf5fbeacdc7a93e39e
arnaz adiputra <arnazadiputra@gmail.com>
2017-04-03 04:20:53 +0700

    initial commit

---
+++ app/routes.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+module.exports = router;
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname, '../about.html'));
+});
+router.get('/contact',function(req,res){
+  res.sendFile(path.join(__dirname, '../contact.html'));
+});
